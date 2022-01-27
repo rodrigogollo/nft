07a2fc61bbb961842fa3801d83a89bc1a427e986
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-01-26 23:12:38 -0300

    resize and added lines param

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 var canvas = document.getElementById("myCanvas");
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
 var context = canvas.getContext("2d");
-// context.moveTo(0, 100);
-// context.lineTo(100, 100)
-// context.stroke();
 
-function drawLine (xx=0, xy=0, yx=0, yy=0, index=0) {
+function drawLinesRandom (index=0, lines=10) {
     if(index == 0) context.clearRect(0, 0, canvas.width, canvas.height);
-    if(index > 100) return;
-    var random1 = Math.floor(Math.random() * 500);
-    var random2 = Math.floor(Math.random() * 500);
-    var random3 = Math.floor(Math.random() * 500);
-    var random4 = Math.floor(Math.random() * 500);
-    context.beginPath();
-    context.moveTo(random1, random2);
-    context.lineTo(random3, random4);
-    context.lineWidth = 10;
-    var randomColor = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
-    context.strokeStyle = randomColor
-    context.stroke();
+    if(index >= lines) {
+        return;
+    }
+    else {
+        context.beginPath();
 
-    drawLine(xx+10, xy + 10, yx+ 10, yy + 10, index+1)
+        //random path
+        var random1 = Math.floor(Math.random() * canvas.width);
+        var random2 = Math.floor(Math.random() * canvas.height);
+        var random3 = Math.floor(Math.random() * canvas.width);
+        var random4 = Math.floor(Math.random() * canvas.height);
+        context.moveTo(random1, random2);
+        context.lineTo(random3, random4);
+    
+        //random line width
+        var randomLineWidth = Math.floor(Math.random() * 100)
+        context.lineWidth = randomLineWidth;
+        
+        //random line color
+        var randomColor = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
+        context.strokeStyle = randomColor;
+        
+        context.stroke();
+        return drawLinesRandom(index+1, lines)
+    }
+    
 }
 
 const HSLToRGB = (h, s, l) => {
@@ -32,6 +44,9 @@ const HSLToRGB = (h, s, l) => {
     return 'rgb('+[255 * f(0) + ',' + 255 * f(8) +',' + 255 * f(4)] + ')';
 };
 
-canvas.addEventListener('click', drawLine, false);
+drawLinesRandom(0, 100);
 
-setInterval(() => {canvas.click()}, 1000); //10ms = crazy 
+canvas.addEventListener('click', function(e){
+    drawLinesRandom(0, 100)
+});
+//setInterval(() => {canvas.click()}, 100); //10ms = crazy 
