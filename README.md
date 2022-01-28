c78ff3d3df11c74947b6269a3117ad8738cac722
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-01-28 00:08:26 -0300

    added new function drawJointedLines, and started drawCircle function

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # NFT
 
 ## my NFT / Generative Art playground
+
+
+### Ideas
+- p5.js
+- Random Book title/author
+- Random country
+- Random images based on the book name
--- script.js
+++ script.js
@@ -1,39 +1,15 @@
 var canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth - 100;
-canvas.height = window.innerHeight - 100;
+canvas.width = 3000
+canvas.height = 3000
+var ctx = canvas.getContext("2d");
 
-var context = canvas.getContext("2d");
+//drawLinesRandom(0, 1000);
+//drawJointedLines(0, 0, 0, 100)
 
-function drawLinesRandom (index=0, lines=10) {
-    canvas.style.background = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
-    if(index == 0) context.clearRect(0, 0, canvas.width, canvas.height);
-    if(index >= lines) {
-        return;
-    }
-    else {
-        context.beginPath();
-
-        //random path
-        var random1 = Math.floor(Math.random() * canvas.width);
-        var random2 = Math.floor(Math.random() * canvas.height);
-        var random3 = Math.floor(Math.random() * canvas.width);
-        var random4 = Math.floor(Math.random() * canvas.height);
-        context.moveTo(random1, random2);
-        context.lineTo(random3, random4);
-    
-        //random line width
-        var randomLineWidth = Math.floor(Math.random() * 0.0001)
-        context.lineWidth = randomLineWidth;
-        
-        //random line color
-        var randomColor = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
-        context.strokeStyle = randomColor;
-        
-        context.stroke();
-        return drawLinesRandom(index+1, lines)
-    }
-    
-}
+canvas.addEventListener('click', function(e){
+    run();
+});
+//setInterval(() => {canvas.click()}, 100); //10ms = crazy 
 
 const HSLToRGB = (h, s, l) => {
     s /= 100;
@@ -45,9 +21,79 @@ const HSLToRGB = (h, s, l) => {
     return 'rgb('+[255 * f(0) + ',' + 255 * f(8) +',' + 255 * f(4)] + ')';
 };
 
-drawLinesRandom(0, 100);
+function drawLinesRandom (index=0, lines=10) {
+    canvas.style.background = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
+    if(index == 0) ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if(index >= lines) {
+        return;
+    }
+    else {
+        ctx.beginPath();
 
-canvas.addEventListener('click', function(e){
-    drawLinesRandom(0, 100)
-});
-//setInterval(() => {canvas.click()}, 100); //10ms = crazy 
+        //random path
+        var random1 = Math.floor(Math.random() * canvas.width);
+        var random2 = Math.floor(Math.random() * canvas.height);
+        var random3 = Math.floor(Math.random() * canvas.width);
+        var random4 = Math.floor(Math.random() * canvas.height);
+        ctx.moveTo(random1, random2);
+        ctx.lineTo(random3, random4);
+    
+        //random line width
+        var randomLineWidth = Math.floor(Math.random() * 10)
+        ctx.lineWidth = randomLineWidth;
+        
+        //random line color
+        var randomColor = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
+        ctx.strokeStyle = randomColor;
+        
+        ctx.stroke();
+        return drawLinesRandom(index+1, lines)
+    }
+    
+}
+
+function drawJointedLines (x, y, index=0, lines=10) {
+    //canvas.style.background = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
+    if(index == 0) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    if(index >= lines) {
+        return;
+    }
+    else {
+        ctx.beginPath();
+
+        //random path
+        var random3 = (100 + Math.floor(Math.random() * canvas.width));
+        var random4 = (100 + Math.floor(Math.random() * canvas.height));
+        ctx.moveTo(x, y);
+        ctx.lineTo(random3, random4);
+        ctx.lineCap = 'round';
+    
+        //random line width
+        var randomLineWidth = Math.floor(Math.random() * 100)
+        ctx.lineWidth = randomLineWidth;
+        
+        
+        //random line color
+        var randomColor = HSLToRGB(Math.floor(Math.random() * 360),  70, 50);
+        ctx.strokeStyle = randomColor;
+        
+        ctx.stroke();
+        return drawJointedLines(random3, random4, index+1, lines)
+    }
+
+}
+
+function drawCircle() {
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+
+function run() {
+    //drawLinesRandom(0, 1000)
+    //drawJointedLines(100, 100, 0, 50);
+    drawCircle();
+}
